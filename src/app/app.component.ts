12cf4cf78190f9c0daadb7f59f7cef1fc753dd01
huertafhm <huerta.fhm@gmail.com>
2018-09-02 12:16:58 +1000

    Log in page added
    Log in page added. Still missing to add functionality for database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'MedApp';
+  loggedUser = true;
+  
 }
