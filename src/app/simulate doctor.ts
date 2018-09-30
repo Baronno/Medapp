52d34a07c624f67e4b695cb446a9ed0ab25d32a8
huertafhm <huerta.fhm@gmail.com>
2018-09-30 23:52:35 +1000

    Changed favicon

--- src/app/simulate doctor.ts
+++ src/app/simulate doctor.ts
@@ -1,7 +1,7 @@
 ﻿import { Doctor } from './doctor';
 
 export const DOCTORS: Doctor[] = [
-    { id: 101, email: 'example@mail.com', password: 'password', name: 'Dr. Martin Brown', phone: 488456841, specialty: 'Cardiology' },
+    { id: 101, email: 'a', password: 'a', name: 'Dr. Martin Brown', phone: 488456841, specialty: 'Cardiology' },
     { id: 102, email: 'example2@mail.com', password: 'password', name: 'Dr. Natalie Davis', phone: 422458454, specialty: 'Neurology' },
     { id: 103, email: 'example3@mail.com', password: 'password', name: 'Dr. Abraham Wilson', phone: 466855423, specialty: 'Radiology' }
 ]
