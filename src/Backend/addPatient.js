628ff71e49d176817ed3c429d479c915aa9a0c25
huertafhm <huerta.fhm@gmail.com>
2018-10-08 11:04:46 +1100

    added backend folder

---
+++ src/Backend/addPatient.js
@@ -0,0 +1,15 @@
+var Patient = require('./patient');
+
+module.exports = function(id, doctorid, name, age, phone, description) {
+    var mongoose = require('mongoose');
+
+    mongoose.connect('mongodb://localhost:27017/Medapp', { useNewUrlParser: true });
+
+    var patient = new Patient({ id : id, doctorid : doctorid, name : name, age : age, phone : phone, description : description });
+
+    patient.save(function (err) {
+      if (err) { throw err; }
+      console.log('patient added');
+      mongoose.connection.close();
+    });
+}
---
+++ src/Backend/patient.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var patientSchema = new mongoose.Schema({
+  id : Number,
+  doctorid : Number,
+  name : String,
+  age : Number,
+  phone : String,
+  description : String
+});
+
+module.exports = mongoose.model('patient', patientSchema);
